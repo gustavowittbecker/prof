1e76d405972531e6aebfad3aa456af8a0994037b
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2023-06-01 14:06:17 -0300

    puerto 3306

--- Php/ejer26DBAbm/datosConexionBase.php
+++ Php/ejer26DBAbm/datosConexionBase.php
@@ -1,6 +1,6 @@
 <?php
 $dbname="b3c1dnzjfajzfextbbcs";
-$host="b3c1dnzjfajzfextbbcs-mysql.services.clever-cloud.com";
+$host="b3c1dnzjfajzfextbbcs-mysql.services.clever-cloud.com:3306";
 $user ="ubtjuyglm1htsjy0";
 $password = "yzK5Hjq3yudhhhfTdyJ2";
 
