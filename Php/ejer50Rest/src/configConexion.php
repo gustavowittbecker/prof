319220bad1b7fd0355c54808fb342a7afb997636
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2023-06-02 10:52:34 -0300

    actualizacion01 ejer50rest

--- Php/ejer50Rest/src/configConexion.php
+++ Php/ejer50Rest/src/configConexion.php
@@ -1,10 +1,11 @@
 <?php
-
+/*Vamos a declarar una variable de tipo array asociativo que contenga 
+los elementos necesarios para definir la fuente de datos*/
 $db = [
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'db' => 'blog' //Cambiar al nombre de tu base de datos
+    'host' => 'b3c1dnzjfajzfextbbcs-mysql.services.clever-cloud.com',
+    'username' => 'ubtjuyglm1htsjy0',
+    'password' => 'yzK5Hjq3yudhhhfTdyJ2',
+    'db' => 'b3c1dnzjfajzfextbbcs' //Cambiar al nombre de tu base de datos
 ];
 
 ?>
