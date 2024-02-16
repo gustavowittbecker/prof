b7cd6afc79697a8c267205fda897e002c8658f95
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2024-02-16 13:46:39 -0300

    ajax corregido con fetch

--- Php/ejer17Ajax/respuesta.php
+++ Php/ejer17Ajax/respuesta.php
@@ -1,9 +1,11 @@
 <?php
-sleep(1); /* Un segundo de delay */
+sleep(3); /* Un segundo de delay */
+
 echo "Request_method: ";
 echo $_SERVER['REQUEST_METHOD'] . "</br>";
 echo "Clave:  ";
 echo  $_POST['clave'];
+
 
 echo "<h4>Clave encriptada en md5 (128 bits o 16 pares hexadecimales): </h4>";
 $claveEncriptada = md5($_POST['clave']);
@@ -13,4 +15,6 @@ echo $claveEncriptada;
 echo "<h4>Clave encriptada en sha1 (160 bits o 20 pares hexadecimales): </h4>";
 $claveEncriptada = sha1($_POST['clave']);
 echo $claveEncriptada;
+
+
 ?>
