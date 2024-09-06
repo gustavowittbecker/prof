07ea41b62567e703bd18114a42ea4b7ee25ccf8b
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2024-09-06 14:40:47 -0300

    tango

--- tango/index.php
+++ tango/index.php
@@ -19,9 +19,11 @@
 	h3 {font-size:2em;text-align: left;}
 	.font24{font-size:24px} /*Para aplicar a un span por ejemplo*/
 	img {
-	    max-width: 100%; /*Ancho libre permitiendo al navegador calcularla en base o otros paramentros sin sobrepasar en ancho el tamaño de la caja*/
+		/*
+	    max-width: 100%; //Ancho libre permitiendo al navegador calcularla en base o otros paramentros sin sobrepasar en ancho el tamaño de la caja
     	max-height: 100%;  
-		margin-bottom:1vh;/*Para que cuando en dispositivos moviles se apilen, no se peguen unas con otras*/
+		margin-bottom:1vh; //Para que cuando en dispositivos moviles se apilen, no se peguen unas con otras
+		*/
 	}
 
 
@@ -30,12 +32,16 @@
 	div.al60 {width:60%;/*background-color:gainsboro*/;float:left;padding: 2vw} /*va a ser un contenedor*/
 	div.logosImg {clear:both;height:5vh;margin-bottom:2vh;/*background-color:lightblue*/}
 	div.logosTexto{height:5vh;float:left;margin-bottom:2vh;font-size:2em/*background-color: lightblue*/;
-		display:flex;align-items: center;}
+		display:flex;align-items: center}
 	
 	div.aparte{clear:both;height:18px;/*background-color:blue*/}
 
 	div.enlace{clear:both;height:5vh;}
 	div.enlace a{font-size: 2em}
+	div.contenedorImagen{width:33%;height:180px;border-style:none;float:left}
+
+	img.imagenBiblioteca{max-width:100%;max-height:100%}
+	div.bloque{display:block}
 </style>
 </Head>
 
@@ -55,34 +61,76 @@
 
 	<h2>Aprendé <span class="font24">a dominar tu cuerpo, a escuchar la música, a seguirla y sobre todo a hacer sentir bien a tu pareja.</span></h2>
 
-	<img src="./imagenes/baileTango03jpeg.jpeg" />
-	<img src="./imagenes/paso02.jpeg" />
-	<img src="./imagenes/post01.jpg" />
+	<div class="bloque">
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/baileTango03jpeg.jpeg" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/paso02.jpeg" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/post01.jpg" />
+	</div>
+	</div>
 
-	<div class="aparte"></div>
-	<hr />
+
 
 	<h2>Aprendé <span class="font24">a pararte bien, a caminar y a desplazarte correctamente y nota que no solo para el baile te será útil.</span></h2>
-	<img src="./imagenes/post02.jpg" />
-	<img src="./imagenes/post03.png" />
-	<img src="./imagenes/post04.jpeg" />
 
 	<div class="aparte"></div>
-	<hr />
+	<div class="bloque">
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/post02.jpg" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/post03.png" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/post04.jpeg" />
+	</div>
+	</div>
 
 	<h2>Aprendé <span class="font24"> a marcar un abrazo firme y a la vez flexible.</span></h2>
-	<img src="./imagenes/abrazo01.png" />
-	<img src="./imagenes/abrazo02.jpeg" />
-	<img src="./imagenes/abrazo03.jpeg" />
-
 
 	<div class="aparte"></div>
-	<hr />
+	<div class="bloque">
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/abrazo01.png" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/abrazo02.jpeg" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/abrazo03.jpeg" />
+	</div>
+	</div>
+
+	
 
 	<h2>Salí a bailar<span class="font24"> pensando que tu baile no es ni mejor ni peor que el de los demás. Es diferente, especial y tal vez único.</span></h2>
-	<img src="./imagenes/unico01.png" />
-	<img src="./imagenes/unico02.png" />
-	<img src="./imagenes/unico03.png" />
+
+
+	<div class="bloque">
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/unico01.png" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/unico02.png" />
+	</div>
+	<div class="contenedorImagen">
+		<img class="imagenBiblioteca" src="./imagenes/unico03.png" />
+	</div>
+	</div>
+
+	<div class="aparte"></div>
+<!--
+	
+
+
+	<h2></h2>
+	<img src="" />
+	<img src="" />
+	<img src="" />
 	<img src="./imagenes/unico04.png" />
 
 	<div class="aparte"></div>
@@ -98,7 +146,7 @@
 
 	<div class="al60">
 
-
+-->
 	<div class="logosImg"><img src="./imagenes/wahtsapp01.png" /></div><div class="logosTexto">&nbsp;15 65069142 (Gustavo)</div>
 	
 
